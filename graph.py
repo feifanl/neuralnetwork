f4d402153790d95352cc0d202f7793cfdfadc781
Feifan Liu <159feifanliu@gmail.com>
2026-03-07 16:58:55 -0600

    start Neuron class and sketch out graphing work

---
+++ graph.py
@@ -0,0 +1,10 @@
+from graphviz import Digraph
+
+def build(node, nodes, edges):
+    pass
+
+def trace(root):
+    pass
+
+def draw_graph():
+    pass
--- nn.py
+++ nn.py
@@ -1,22 +1,28 @@
+import numpy as np
+
 # Using normal scalars doesn't work; backprop requires knowing operations and nodes that produced certain values
 class Value: 
-    def __init__(self, v):
+    def __init__(self, v, _children, _op, label = ""):
         self.v = v
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(v = {self.v})"
 
     def __add__(self, other):
-        res = Value(self.v + other.v)
+        res = Value(self.v + other.v, (self, other), '+')
         return res
 
     def __mul__(self, other):
-        res = Value(self.v * other.v)
+        res = Value(self.v * other.v, (self, other), '*')
         return res
 
-class Node: 
-    def __init__(self):
-        pass
+class Neuron:
+    def __init__(self, n_weights):
+        self.weights = [Value(np.random.rand(), (), '', f'w{i}') for i in range(n_weights)]
+        self.bias = Value(np.random.rand(), (), '', 'b')
 
 class Layer: 
     def __init__(self):
